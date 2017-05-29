1ef0e67313e90712215d38707939f7b2e90a3d8d
Tracy <ismail6666@gmail.com>
2017-05-30 02:33:57 +0500

    Create GrblLCD.h

---
+++ GrblLCD.h
@@ -0,0 +1,17 @@
+/* 
+This file is created to add lcd functionality to GRBL using arduino mega 2560 only.
+Using 20x4 LCD DISPLAY
+*/
+
+#ifndef GrblLCD_h
+#define GrblLCD_h
+
+#define LCD_PORT PORTL
+#define RS       0
+#define EN       1
+#define DB4      2
+#define DB5      3
+#define DB6      4
+#define DB7      5
+
+#endif
